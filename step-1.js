ea369e442fb32ce8e5c4e71747d9633b1bd9915c
eunjee <940116e@naver.com>
2019-12-01 03:19:00 +0900

    1단계: 간단 야구 게임 구현하기
    1단계 요구사항에 따라 코드 작성함
    
    1. 게임이 시작되면 "첫 번째 타자가 타석에 입장했습니다." 메시지와 함께 경기를 진행.
    2. 경기가 진행되면 랜덤하게 스트라이크, 볼, 안타, 아웃 네가지 중 한 결과 출력되게 하기.
    3. 2번의 결과의 아래 줄에 누적된 스트라이크(S), 볼(B), 아웃(O) 상황을 출력.
    4. 스트라이크 3회 누적되면 1 아웃되게 할 것.
    5. 볼이 4회 누적되면 1 안타가 되게 할 것.
    6. 4번과 5의 경우를 포함한 안타 또는 아웃의 경우 "다음 타자가 타석에 입장했습니다." 메시지와 함께 경기 이어지게 할 것.
    7. 다음 타자의 차례에서 현재의 안타, 아웃 카운트는 유지되고 스트라이크와 볼 카운트는 초기화된다.
    8. 3OUT시 전체 안타수를 출력하고 경기 종료되게 할 것.

---
+++ step-1.js
@@ -0,0 +1,63 @@
+console.log('신나는 야구 게임! \n첫 번째 타자가 타석에 입장했습니다.');
+
+const RESULT = ['스트라이크', '볼', '안타'];
+
+let strike = 0;
+let ball = 0;
+let hit = 0;
+let out = 0;
+
+const baseBallGame = (result) => {
+  const random = Math.floor(Math.random() * result.length) + 0;
+  const resultRandom = result[random];
+
+  const STRIKE = () => {
+    const STRIKECOUNT = 3;
+    console.log('스트라이크!');
+    ++strike;
+    if (strike >= STRIKECOUNT) OUT();
+  }
+
+  const BALL = () => {
+    const BALLCOUNT = 4;
+    console.log('볼!');
+    ++ball;
+    if (ball >= BALLCOUNT) HIT();
+  }
+
+  const OUT = () => {
+    const MAXOUTCOUNT = 3;
+    if ((out + 1) < MAXOUTCOUNT) {
+      console.log('아웃! 다음 타자가 타석에 입장했습니다.');
+      reset();
+      ++out;
+    } else {
+      console.log(`최종안타수 : ${hit}\nGAME OVER`);
+      reset();
+      ++out;
+      clearInterval(clear);
+    }
+  }
+
+  const HIT = () => {
+    console.log('안타! 다음 타자가 타석에 입장했습니다.');
+    ++hit;
+    reset();
+  }
+
+  const reset = () => {
+    strike = 0;
+    ball = 0;
+  }
+
+  if (resultRandom === '스트라이크') STRIKE();
+  else if (resultRandom === '볼') BALL();
+  else if (resultRandom === '안타') HIT();
+
+  return `${strike}S ${ball}B ${out}O ${hit}Hits`;
+}
+
+const clear = setInterval( ()=> {
+  console.log(baseBallGame(RESULT));
+}, 100);
+
