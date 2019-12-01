c7600898bda4f4d4471497cd747de07e285eb23d
eunjee <940116e@naver.com>
2019-12-01 17:25:38 +0900

    게임시작 메시지 추가
    게임시작 알람 추가함
    
    랜덤함수 뒤에 붙는 +0 삭제

--- step-1.js
+++ step-1.js
@@ -1,34 +1,35 @@
-console.log('신나는 야구 게임! \n첫 번째 타자가 타석에 입장했습니다.');
+alert("게임을 시작하겠습니까?");
+console.log("신나는 야구 게임! \n첫 번째 타자가 타석에 입장했습니다.");
 
-const RESULT = ['스트라이크', '볼', '안타'];
+const RESULT = ["스트라이크", "볼", "안타"];
 
 let strike = 0;
 let ball = 0;
 let hit = 0;
 let out = 0;
 
-const baseBallGame = (result) => {
-  const random = Math.floor(Math.random() * result.length) + 0;
+const baseBallGame = result => {
+  const random = Math.floor(Math.random() * result.length);
   const resultRandom = result[random];
 
   const STRIKE = () => {
     const STRIKECOUNT = 3;
-    console.log('스트라이크!');
+    console.log("스트라이크!");
     ++strike;
     if (strike >= STRIKECOUNT) OUT();
-  }
+  };
 
   const BALL = () => {
     const BALLCOUNT = 4;
-    console.log('볼!');
+    console.log("볼!");
     ++ball;
     if (ball >= BALLCOUNT) HIT();
-  }
+  };
 
   const OUT = () => {
     const MAXOUTCOUNT = 3;
-    if ((out + 1) < MAXOUTCOUNT) {
-      console.log('아웃! 다음 타자가 타석에 입장했습니다.');
+    if (out + 1 < MAXOUTCOUNT) {
+      console.log("아웃! 다음 타자가 타석에 입장했습니다.");
       reset();
       ++out;
     } else {
@@ -37,27 +38,26 @@ const baseBallGame = (result) => {
       ++out;
       clearInterval(clear);
     }
-  }
+  };
 
   const HIT = () => {
-    console.log('안타! 다음 타자가 타석에 입장했습니다.');
+    console.log("안타! 다음 타자가 타석에 입장했습니다.");
     ++hit;
     reset();
-  }
+  };
 
   const reset = () => {
     strike = 0;
     ball = 0;
-  }
+  };
 
-  if (resultRandom === '스트라이크') STRIKE();
-  else if (resultRandom === '볼') BALL();
-  else if (resultRandom === '안타') HIT();
+  if (resultRandom === "스트라이크") STRIKE();
+  else if (resultRandom === "볼") BALL();
+  else if (resultRandom === "안타") HIT();
 
   return `${strike}S ${ball}B ${out}O ${hit}Hits`;
-}
+};
 
-const clear = setInterval( ()=> {
+const clear = setInterval(() => {
   console.log(baseBallGame(RESULT));
-}, 100);
-
+}, 300);
