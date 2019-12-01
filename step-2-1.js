821113e5c2f00e14ee4e7718ba4cdcdb7a71fc2d
eunjee <940116e@naver.com>
2019-12-01 23:28:18 +0900

    2단계(1)번 문제 요구사항에 따라 코드 작성함
    2단계 요구사항 1: 팀데이터 입력 및 저장
    
    1. 두 야구팀의 팀이름과 서수 데이터를 입력
    2. 각 팀은 9명의 타자와 한명의 투수로 이루어진다.
    3. 타자 정보: 순번, 이름, 타율을 저장한다.
    4. 타율이란 각 타자가 안타를 칠 수 있는 확률로 타율 h는 0.1>h>0.5
    5. 타율은 소수 셋째 자리까지 입력할 것
    6. 입력한 팀 데이터에 대한 저장하기 및 출력하기 기능을 구현한다.
    7. 편의를 위해 에러 처리 등의 기능을 구현할 것
    
    4,5번 해결 중!!

---
+++ step-2-1.js
@@ -0,0 +1,40 @@
+function main() {
+    const MEMBER = 9;
+    let firstTeamInfo = "";
+    let firstTeamData = [];
+  
+    let secondTeamInfo = "";
+    let secondTeamData = [];
+  
+    const firstTeamName = prompt("1팀의 이름을 입력하세요.");
+  
+    for (let i = 0; i <= MEMBER; i++) {
+      const name = firstTeamInfo.split(",")[0] || "1번팀 투수";
+      const average = firstTeamInfo.split(",")[1] || "투수";
+  
+      firstTeamInfo = prompt(
+        `${i +
+          1}번 타자 정보 입력 - ex) 권은지, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5`
+      );
+      firstTeamData.push({ id: i, name, average });
+    }
+  
+    const secondTeamName = prompt("2팀의 이름을 입력하세요.");
+  
+    for (let i = 0; i <= MEMBER; i++) {
+      const name = secondTeamInfo.split(",")[0] || "2번팀 투수";
+      const average = secondTeamInfo.split(",")[1] || "투수";
+      secondTeamInfo = prompt(
+        `${i}번 타자 정보 입력 - ex) 권은정, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5`
+      );
+      secondTeamData.push({ id: i, name, average });
+    }
+  
+    console.log(firstTeamName);
+    console.log(firstTeamData);
+  
+    console.log(secondTeamName);
+    console.log(secondTeamData);
+  }
+  main();
+  
