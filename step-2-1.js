423f6202369971dc85ec9ffd4856959bae243167
eunjee <940116e@naver.com>
2019-12-02 12:53:22 +0900

    2단계(1)번 문제 해결 중
    4. 타율이란 각 타자가 안타를 칠 수 있는 확률로 타율 h는 0.1>h>0.5
    5. 타율은 소수 셋째 자리까지 입력할 것
    
    소숫점 3째자리까지 .toFixed(3)를 이용하여 진행 중....

--- step-2-1.js
+++ step-2-1.js
@@ -1,11 +1,12 @@
 function main() {
-    const MEMBER = 9;
+    const MEMBER = 2; //9
     let firstTeamInfo = "";
     let firstTeamData = [];
   
     let secondTeamInfo = "";
     let secondTeamData = [];
   
+    console.log(secondTeamData[3]);
     const firstTeamName = prompt("1팀의 이름을 입력하세요.");
   
     for (let i = 0; i <= MEMBER; i++) {
@@ -14,7 +15,7 @@ function main() {
   
       firstTeamInfo = prompt(
         `${i +
-          1}번 타자 정보 입력 - ex) 권은지, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5`
+          1}번 타자 정보 입력 - ex) 권은지, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5. 타율은 소숫점 셋째자리까지 입력가능.`
       );
       firstTeamData.push({ id: i, name, average });
     }
@@ -24,6 +25,7 @@ function main() {
     for (let i = 0; i <= MEMBER; i++) {
       const name = secondTeamInfo.split(",")[0] || "2번팀 투수";
       const average = secondTeamInfo.split(",")[1] || "투수";
+  
       secondTeamInfo = prompt(
         `${i}번 타자 정보 입력 - ex) 권은정, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5`
       );
@@ -36,5 +38,22 @@ function main() {
     console.log(secondTeamName);
     console.log(secondTeamData);
   }
-  main();
   
+  // main();
+  // function ex(){
+  //   var obj = {
+  //     "1": 0.33333,
+  //     "2": "권은지"
+  //   };
+  
+  //   var first_key = Object.keys(obj)[0];
+  //   var first_value = obj[Object.keys(obj)[0]].toFixed(3);
+  //   var first_rangeValue = 0.1 > obj[Object.keys(obj)[0]] > 0.5;
+  //   if (0.1 > first_rangeValue && first_rangeValue > 0.5) {
+  //     console.log("222");
+  //   }
+  //   console.log(first_key);
+  //   console.log(first_value);
+  //   console.log(first_rangeValue);
+  // }
+  // console.log(ex());
