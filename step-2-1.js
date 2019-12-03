a02fa19a81168dc24035e2833ccd593b9fd66120
eunjee <940116e@naver.com>
2019-12-03 13:00:14 +0900

    2단계(1)번
    4, 5번 해결 중..

--- step-2-1.js
+++ step-2-1.js
@@ -12,12 +12,13 @@ function main() {
     for (let i = 0; i <= MEMBER; i++) {
       const name = firstTeamInfo.split(",")[0] || "1번팀 투수";
       const average = firstTeamInfo.split(",")[1] || "투수";
-  
+      // alert(Math.toFixed(3))
       firstTeamInfo = prompt(
         `${i +
           1}번 타자 정보 입력 - ex) 권은지, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5. 타율은 소숫점 셋째자리까지 입력가능.`
       );
       firstTeamData.push({ id: i, name, average });
+      // firstTeamData[Object.keys(average)[1]].toFixed(3);
     }
   
     const secondTeamName = prompt("2팀의 이름을 입력하세요.");
@@ -30,6 +31,7 @@ function main() {
         `${i}번 타자 정보 입력 - ex) 권은정, 0.499 \n단, 확률로 타율(h)은 0.1 < h < 0.5`
       );
       secondTeamData.push({ id: i, name, average });
+      // average.Math.toFixed(3);
     }
   
     console.log(firstTeamName);
@@ -39,21 +41,4 @@ function main() {
     console.log(secondTeamData);
   }
   
-  // main();
-  // function ex(){
-  //   var obj = {
-  //     "1": 0.33333,
-  //     "2": "권은지"
-  //   };
-  
-  //   var first_key = Object.keys(obj)[0];
-  //   var first_value = obj[Object.keys(obj)[0]].toFixed(3);
-  //   var first_rangeValue = 0.1 > obj[Object.keys(obj)[0]] > 0.5;
-  //   if (0.1 > first_rangeValue && first_rangeValue > 0.5) {
-  //     console.log("222");
-  //   }
-  //   console.log(first_key);
-  //   console.log(first_value);
-  //   console.log(first_rangeValue);
-  // }
-  // console.log(ex());
+  main();
